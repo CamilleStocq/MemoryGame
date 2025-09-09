c3f2db9243a7ae3827e134b1a9d09a1e92700399
CamilleStocq <95492172+CamilleStocq@users.noreply.github.com>
2025-09-09 09:21:54 +0200

    put doubles but no more

--- #Project/Scripts/CardManager.cs
+++ #Project/Scripts/CardManager.cs
@@ -11,14 +11,34 @@ public class CardManager : MonoBehaviour
         this.colors = colors; //constructeur
         this.deck = deck; //constructeur
 
-        // List<Color> doubleColor = new List<Color>();
+        List<Color> doubleColor = new List<Color>();
 
-        int colorIndex;
-        for (int index = 0; index < deck.Count; index++)
+
+        // int colorIndex;
+        // for (int index = 0; index < deck.Count; index++)
+        // {
+        //     colorIndex = Random.Range(0, colors.Length);
+        //     deck[index].Initialize(colors[colorIndex], colorIndex, this); // this pour se passer sois meme
+
+        // }
+
+        for (int index = 0; index < deck.Count / 2; index++)
         {
-            colorIndex = Random.Range(0, colors.Length);
-            deck[index].Initialize(colors[colorIndex], colorIndex, this); // this pour se passer sois meme
+            doubleColor.Add(colors[index]);
+            doubleColor.Add(colors[index]);
+        }
 
+        for (int i = 0; i < doubleColor.Count; i++)
+        {
+            Color temp = doubleColor[i];
+            int randomIndex = Random.Range(i, doubleColor.Count);
+            doubleColor[i] = doubleColor[randomIndex];
+            doubleColor[randomIndex] = temp;
+        }
+
+        for (int j = 0; j < deck.Count; j++)
+        {
+            deck[j].Initialize(doubleColor[j], j, this);
         }
     }
 }
